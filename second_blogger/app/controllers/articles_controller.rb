798775e36205479e934d9962b467bebddf6c1465
Alex Hardt <hardt.alex@gmail.com>
2015-03-02 09:48:23 -0600

    add css stylesheet, create article show controller/view/model

--- second_blogger/app/controllers/articles_controller.rb
+++ second_blogger/app/controllers/articles_controller.rb
@@ -3,4 +3,8 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def show#(id) #how is the id arg passed in?
+    @article = Article.find(params[:id])
+  end
 end
