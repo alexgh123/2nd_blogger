8a4af8771c7303e32e32bd2163d0d327b600ecad
Alex Hardt <hardt.alex@gmail.com>
2015-03-02 11:17:19 -0600

    flash notices for destroy and create

--- second_blogger/app/controllers/articles_controller.rb
+++ second_blogger/app/controllers/articles_controller.rb
@@ -17,12 +17,18 @@ class ArticlesController < ApplicationController
   def create
     @article = Article.new(article_params)
     @article.save
+
+    flash.notice = "Article '#{@article.title}' Created!" #slightly silly here b/c user will see the newly created article
+
     redirect_to article_path(@article)
   end
 
   def destroy
     @article = Article.find(params[:id])
     @article.destroy
+
+    flash.notice = "Article '#{@article.title}' Deleted!"
+
     redirect_to articles_path
   end
 
