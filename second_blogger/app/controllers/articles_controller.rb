816f6379cae8e0a5bfe9b559be2f9c90fbcdd074
Alex Hardt <hardt.alex@gmail.com>
2015-03-02 11:14:39 -0600

    lots of updates here with update, edit commands
    also added flash notice with update

--- second_blogger/app/controllers/articles_controller.rb
+++ second_blogger/app/controllers/articles_controller.rb
@@ -1,10 +1,41 @@
 class ArticlesController < ApplicationController
+  include ArticlesHelper
 
   def index
     @articles = Article.all
   end
 
-  def show#(id) #how is the id arg passed in?
+  def show
     @article = Article.find(params[:id])
+  end
+  #this is a somewhat confusing concept about when/how args are passed in, its hard to keep track of and not explicitly showed
+
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+    @article.save
+    redirect_to article_path(@article)
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+  end
+
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
+
+    flash.notice = "Article '#{@article.title}' Updated!"
+
+    redirect_to articles_path(@article)
   end
 end
