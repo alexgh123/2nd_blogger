cce50f074455723af347e890b125b96bbdd34aa9
Alex Hardt <hardt.alex@gmail.com>
2015-03-03 10:57:53 -0600

    removed excess syntax from routes.rb

--- second_blogger/config/routes.rb
+++ second_blogger/config/routes.rb
@@ -3,12 +3,10 @@ Rails.application.routes.draw do #tutorial shows this line to look like: Blogger
   root to: 'articles#index'
 
   resources :articles do
-    resources :comments, :tags
+    resources :comments
   end
 
   resources :tags
 
-  # resources :tags do
-  #   resources :tags
-  # end
+
 end
